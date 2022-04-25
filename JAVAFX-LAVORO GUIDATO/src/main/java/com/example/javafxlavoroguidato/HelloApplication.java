0feb4e2d5d608d09773af95720689e3fa0482c54
Chrisis2004 <christian.canossa04@gmail.com>
2022-04-25 17:41:19 +0200

    lavoro guidato caricamento

--- JAVAFX-LAVORO GUIDATO/src/main/java/com/example/javafxlavoroguidato/HelloApplication.java
+++ JAVAFX-LAVORO GUIDATO/src/main/java/com/example/javafxlavoroguidato/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LEZIONE JAVAFX - LAVORO GIUDATO.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxmlGuidato.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("LEZIONE JAVAFX LAVORO GUIDATO");
         stage.setScene(scene);
--- JAVAFX-LAVORO GUIDATO/src/main/java/com/example/javafxlavoroguidato/LavoroGuidatoController.java
+++ JAVAFX-LAVORO GUIDATO/src/main/java/com/example/javafxlavoroguidato/LavoroGuidatoController.java
@@ -1,14 +1,52 @@
 package com.example.javafxlavoroguidato;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
 
 public class LavoroGuidatoController {
     @FXML
-    private Label welcomeText;
-
+    private Button button;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView image;
+    @FXML
+    private CheckBox checkBox;
+    @FXML
+    private Label label;
+    @FXML
+    private Label label2;
+    @FXML
+    ListView<String> listView = new ListView<String>();
+    @FXML
+    private TextField textField;
+
+    public void initialize() throws IOException {
+        String[] stringDaStampare = new String[4];
+        stringDaStampare[0] = "String per ListView n.1";
+        stringDaStampare[1] = "String per ListView n.2";
+        stringDaStampare[2] = "String per ListView n.3";
+        stringDaStampare[3] = "String per ListView n.4";
+
+        listView.getItems().addAll(stringDaStampare);
+    }
+    public void onButtonClick(){
+        button.setStyle("-fx-background-color: blue");
+    }
+    public void onCheckBoxClick(){
+        if (checkBox.isSelected())
+            image.setVisible(true);
+        else
+            image.setVisible(false);
+    }
+    public void onLabelMouseOver(){
+        label.setText("Sei passato qui sopra");
+    }
+    public void onLabelMouseClick(){
+        label.setText("Mi hai cliccato");
+    }
+    public void onAddText() {
+        label2.setText(textField.getText());
     }
 }
